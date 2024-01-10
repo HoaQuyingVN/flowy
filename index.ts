d30a4d0c925fcb4884f46452032ec9382e380fb9
HoaQuyingVN <126839568+HoaQuyingVN@users.noreply.github.com>
2024-01-10 11:54:42 +0000

    [FLOWY] Automatic changed/added

--- index.ts
+++ index.ts
@@ -1,11 +1,6 @@
-import * as http from 'http';
-const server = http.createServer((_req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-const port = 3000;
-const hostname = "0.0.0.0"
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+/* Flowy - Package manager for NPM(Third-party NPM) by @HoaQuyingVN 
+ * 
+ * Copyright (c) 2024 HoaQuyingVN
+ * Use npm install -g flowy to install
+ */
+import 
